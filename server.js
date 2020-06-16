c265cf08e82fd1ea7dc399102e53e775a7cbdfa3
Samuel Alves <samuelalves681@gmail.com>
2020-06-16 18:54:01 -0300

    correcao app.listen

--- server.js
+++ server.js
@@ -56,5 +56,8 @@ routes.get('/', function(req, res){
     
 
     
-server.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+      console.log(`Our app is running on port ${ PORT }`);
+  });
 
