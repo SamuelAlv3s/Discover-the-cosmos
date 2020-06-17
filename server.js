429139923270b73453836f30a2edf71a18a0a65d
Samuel Alves <samuelalves681@gmail.com>
2020-06-16 21:47:51 -0300

    heroku: add descricao

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ nunjucks.configure('views', {
     noCache: true
 });
 
-const apiKey = '?';
-const url = '?';
+const apiKey = 'PfadJi8ZW5GLeaG8N6gTVZNvkJMf81sQTY7E-0_fJNVo';
+const url = 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/77c87984-e646-4d16-b123-806d334eaef9';
 
 
 routes.get('/', function(req, res){
-  fetch('https://api.nasa.gov/planetary/apod?api_key=?')
+  fetch('https://api.nasa.gov/planetary/apod?api_key=0yDrBxsNT9gQZDsQVx0i26KWg7xHyYfANQakmgFj')
   .then( (res) => {return res.json()})
   .then( (apod) =>{
 
